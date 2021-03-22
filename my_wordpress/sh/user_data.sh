e98aee374cf6512fd137cec889e5998751acc5b4
nisioka <nishioka.daisuke@gmail.com>
2021-03-22 15:14:58 +0900

    軽微修正。

--- my_wordpress/sh/user_data.sh
+++ my_wordpress/sh/user_data.sh
@@ -3,8 +3,8 @@
 #
 # Install required modules.
 #
-apt-get update
-apt-get install -y git docker jq
+yum update
+yum install -y git docker jq
 curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
@@ -16,7 +16,7 @@ SETENV_SHELL="/etc/profile.d/setenv.sh"
 
 # Create initenv.sh
 # initialize environment variable script -START-----------------------
-cat >> "$INITENV_SHELL" <<"__EOS__"
+cat > "$INITENV_SHELL" <<"__EOS__"
 #!/bin/bash
 
 # Environment variables file name
@@ -28,7 +28,6 @@ REGION=$(curl 169.254.169.254/latest/meta-data/placement/region)
 ZONE=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone)
 
 VPC_ID=$(aws ec2 describe-instances --region ${REGION} --instance-ids ${INSTANCE_ID} --query "Reservations[0].Instances[0].NetworkInterfaces[0].VpcId" --output text)
-VPC_NAME=$(aws ec2 describe-vpcs --region ${REGION} --vpc-ids ${VPC_ID} --query "Vpcs[0].Tags[?Key=='Name'].Value" --output text)
 VPC_PROJECT=$(aws ec2 describe-vpcs --region ${REGION} --vpc-ids ${VPC_ID} --query "Vpcs[0].Tags[?Key=='Project'].Value" --output text)
 VPC_ENV=$(aws ec2 describe-vpcs --region ${REGION} --vpc-ids ${VPC_ID} --query "Vpcs[0].Tags[?Key=='Env'].Value" --output text)
 
@@ -39,17 +38,16 @@ cat > "${SETENV_SHELL}" <<EOF
 #
 # [$(date '+%Y-%m-%dT%H:%M:%S+09:00' -d '9 hour')] Initialized scripts.
 #
-export INSTANCE_ID=${INSTANCE_ID}
-export REGION=${REGION}
-export ZONE=${ZONE}
-export VPC_ID=${VPC_ID}
-export VPC_NAME="${VPC_NAME}"
+export INSTANCE_ID="${INSTANCE_ID}"
+export REGION="${REGION}"
+export ZONE="${ZONE}"
+export VPC_ID="${VPC_ID}"
 export VPC_PROJECT="${VPC_PROJECT}"
 export VPC_ENV="${VPC_ENV}"
 EOF
 
-for PARAMS in $(echo ${SSM_PARAMETER_STORE} | /usr/local/bin/jq -r '.Parameters[] | .Name + "=" + .Value'); do
-echo "export ${PARAMS##*/}"
+for PARAMS in $(echo ${SSM_PARAMETER_STORE} | jq -r '.Parameters[] | .Name + "=" + .Value'); do
+echo 'export "${PARAMS##*/}"'
 done >> "${SETENV_SHELL}"
 
 chmod +x "$SETENV_SHELL"
